24c1631ed129716aa04611dfa2f1a6a9d14a8158
Attiq <attiq.rao@ilsainteractive.com>
2011-07-18 18:00:55 +0500

    issues fixing..

--- app/models/asset.rb
+++ app/models/asset.rb
@@ -1,6 +1,6 @@
 class Asset < ActiveRecord::Base
-  has_attached_file :asset, :styles => { :medium => "300x300>", :thumb => "100x100>" }
-  validates_attachment_content_type :asset, :content_type => [ 'application/msword', 'application/vnd.openxmlformats-officedocument.wordprocessingml.document' ]
+  has_attached_file :asset, :styles => { :medium => "300x300>", :thumb => "100x100>" },
+  :content_type => [ 'application/msword', 'application/vnd.openxmlformats-officedocument.wordprocessingml.document' ]
   belongs_to :user
 end
 

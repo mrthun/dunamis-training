9f1393c8b3036ccd18176fa15084075fa1efd047
Attiq <attiq.rao@ilsainteractive.com>
2011-07-18 17:27:01 +0500

    fixed some issues

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -131,7 +131,7 @@ class ProfilesController < ApplicationController
       @asset.user = current_user
       @asset.asset_asset_type = "#{params[:key]}"
       if @asset.save && @asset.errors.empty?
-        render :text => @asset.asset.original_filename
+        render :text => "<a href='http://#{request.host_with_port}#{@asset.asset.url}'>"
       else
         render :text => @asset.errors
       end

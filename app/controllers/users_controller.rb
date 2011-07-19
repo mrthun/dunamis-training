0ab42e68add219baf91d1221d5330cdb2576391b
Attiq <attiq.rao@ilsainteractive.com>
2011-07-19 18:16:07 +0500

    fixed an issue..

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -60,7 +60,7 @@ class UsersController < ApplicationController
     success = @user && @user.save
 
     if success && @user.errors.empty?
-      @user.activate!
+      #@user.activate!
       flash[:notice] = "An email has been sent to the created emplyee with activation information."
       redirect_to :action => :list_employees
     else

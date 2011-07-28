2a7092b79446707d34cc50fa2f32b2cc8d894558
Attiq <attiq.rao@ilsainteractive.com>
2011-07-28 16:21:08 +0500

    fixed some subdomina issues

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,17 +2,26 @@ class ApplicationController < ActionController::Base
   include UrlHelper
   include AuthenticatedSystem
   protect_from_forgery
-  before_filter :set_organization
+  before_filter :set_organization_subdomain
   before_filter :set_mailer_url_options
 
   protected
 
-  def set_organization
+  def set_organization_subdomain
     if is_subdomian?(request.subdomain)
-      @organization = User.find_by_name(request.subdomain)
-      unless @organization.present?
-        return render :template => '/shared/404'
+      if logged_in? && current_user.is_admin?
+        redirect_to(request.protocol + request.domain + (request.port.nil? ? '' : ":#{request.port}"))
+      else
+        @organization = User.find_by_name(request.subdomain)
+        unless @organization.present?
+          return render :template => '/shared/404'
+        end
+        @subdomain = @organization.subdomain.name
       end
+    elsif logged_in? && current_user.is_organization?
+      redirect_to(request.protocol + "#{current_user.subdomain.name}." +request.domain + (request.port.nil? ? '' : ":#{request.port}") + request.request_uri)
+    else
+      @subdomain = false
     end
   end
   
